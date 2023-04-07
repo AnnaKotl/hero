eb126b44e353672c06e88c48cdc8a5969b4e307d
Анна Котляр <kotlyaranya1771@gmail.com>
2023-04-07 20:26:49 +0300

    hero

---
+++ modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function toggleModal() {
+    const body = document.querySelector('body');
+    body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
